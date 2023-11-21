f336eca9b5d6aff48513b5183cf4301d50c45450
an2niy85 <an2niy85@gmail.com>
2023-11-21 21:57:00 +0400

    IntroductionToOOP_HW5

---
+++ src/main/java/ru/geekbrains/lesson5/models/Reservation.java
@@ -0,0 +1,32 @@
+package ru.geekbrains.lesson5.models;
+
+import java.util.Date;
+
+public class Reservation {
+
+    private static int counter = 1000;
+    private final int id;
+
+    private final Table table;
+
+    private Date date;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+
+    public Reservation(Table table, Date date, String name) {
+        this.date = date;
+        this.name = name;
+        this.table = table;
+        id = ++counter;
+    }
+
+}
